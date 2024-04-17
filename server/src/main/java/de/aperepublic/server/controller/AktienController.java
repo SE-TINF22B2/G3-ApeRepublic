30953ada91991aa10e1536dda514dd98c8402e8e
Matej <147487525+MatejEvc@users.noreply.github.com>
2024-04-17 12:58:53 +0200

    FinnhubCompanyProfile2 added, nicht funktionsfaehig

--- server/src/main/java/de/aperepublic/server/controller/AktienController.java
+++ server/src/main/java/de/aperepublic/server/controller/AktienController.java
@@ -3,6 +3,7 @@ package de.aperepublic.server.controller;
 import de.aperepublic.server.models.AktieHistorie;
 import de.aperepublic.server.models.AktieInfo;
 import de.aperepublic.server.models.AktiePreis;
+import de.aperepublic.server.services.FinnhubCompanyProfile2Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,8 @@ import java.util.Map;
 
 @RestController
 public class AktienController {
+
+    FinnhubCompanyProfile2Service companyProfile2Service;
 
     @GetMapping("/aktie/info")
     public AktieInfo bedieneAktieInfo() {
@@ -24,7 +27,8 @@ public class AktienController {
     }
 
     @GetMapping("aktie/historie")
-    public AktieHistorie bedieneHistorie(){
+    public AktieHistorie bedieneHistorie() {
+
         Map<LocalDateTime, Double> historyMap = new HashMap<>();
         historyMap.put(LocalDateTime.now(), 150.4);
         historyMap.put(LocalDateTime.now().minusSeconds(1), 153.3);
@@ -40,9 +44,9 @@ public class AktienController {
 
     @PostMapping("/aktie/handeln") //bin mir nicht sicher ob hier ein get kommt
     public Map<String, Object> bedieneAktiePreisEchtzeit(@RequestBody String aktion, @RequestBody String symbol, @RequestParam double menge) {
-        if(aktion.contentEquals("kaufen")) {
+        if (aktion.contentEquals("kaufen")) {
             return Map.of("anfrageStatus", 0, "kaufStatus", true, "symbol", symbol, "menge", menge, "price", 173.52);
-        } else if(aktion.contentEquals("verkaufen")) {
+        } else if (aktion.contentEquals("verkaufen")) {
             return Map.of("anfrageStatus", 0, "verkaufStatus", true, "symbol", symbol, "menge", menge, "price", 173.52);
         } else {
             return Map.of("anfrageStatus", 1);
