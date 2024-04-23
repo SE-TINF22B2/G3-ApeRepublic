bdc3f3259a20eae74713810a04fafc81d80c2281
Marc Schillinger <marcelia@web.de>
2024-04-23 22:13:30 +0200

    FinnhubCompanyProfile2Service Contructor fix in Test

--- server/src/main/java/de/aperepublic/server/services/FinnhubCompanyProfile2Service.java
+++ server/src/main/java/de/aperepublic/server/services/FinnhubCompanyProfile2Service.java
@@ -2,7 +2,6 @@ package de.aperepublic.server.services;
 
 import de.aperepublic.server.models.CompanyProfile2;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +13,7 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class FinnhubCompanyProfile2Service {
     private final String finnhubToken = "cl0cekhr01qs0rbeo410cl0cekhr01qs0rbeo41g";
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     public CompanyProfile2 getCompanyFromIsin(String isin) {
         URI urlCompanyProfile = UriComponentsBuilder.newInstance()
