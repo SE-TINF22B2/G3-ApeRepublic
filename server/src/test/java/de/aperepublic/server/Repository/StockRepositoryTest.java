cc05556ee372ae902397e200906637daca902b16
Anisa Aburass <anisa.aburass@gmail.com>
2024-05-07 11:09:42 +0200

    Stocktest weiter entwickelt, Annotations und Stock Abfrage Test hinzugefügt

--- server/src/test/java/de/aperepublic/server/Repository/StockRepositoryTest.java
+++ server/src/test/java/de/aperepublic/server/Repository/StockRepositoryTest.java
@@ -2,21 +2,32 @@ package de.aperepublic.server.Repository;
 
 import de.aperepublic.server.Entity.Stock;
 import de.aperepublic.server.repositories.StockRepository;
+ import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+//@RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class StockRepositoryTest {
 
+    @Autowired
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Autowired
         private StockRepository sRp;
 
         @Test
+        @Transactional
         public void benutzerSpeichernTest() {
             // Benutzer erstellen
             System.out.println("trying to create STock");
@@ -27,13 +38,24 @@ public class StockRepositoryTest {
             s.setSymbol("AAnA");
             System.out.println("Stock created");
 
-
             // Benutzer speichern
-            Stock gespeicherterBenutzer = sRp.save(s);
+            Stock gespeicherterBenutzer = sRp.saveAndFlush(s);
+            sRp.flush();
+
             System.out.println("Stock Saved");
+            System.out.println("gespeicherter User: " + gespeicherterBenutzer.toString());
+            System.out.println("Bneutzer aus DB: " + sRp.getReferenceById(s.getISIN()).toString());
             // Überprüfen, ob die ID des gespeicherten Benutzers nicht null ist
-            assertNotNull(s.getISIN());
+          //  assertNotNull(gespeicherterBenutzer.getISIN());
+            assertEquals(s, sRp.getReferenceById(s.getISIN()));
         }
+
+        @Test
+        @Transactional
+        public void stockAbfragen(){
+            assertNotNull(sRp.getReferenceById("PURRRRRRR"));
+        }
+
 
     }
 
