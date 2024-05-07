46f93a0b07a8a7195c56732bd9e82c566f5fef69
Anisa Aburass <anisa.aburass@gmail.com>
2024-05-07 10:47:29 +0200

    Repository für User Klasse angelegt

--- server/src/main/java/de/aperepublic/server/repositories/UsersRepository.java
+++ server/src/main/java/de/aperepublic/server/repositories/UsersRepository.java
@@ -1,2 +1,9 @@
-package de.aperepublic.server.repositories;public class UsersRepository {
+package de.aperepublic.server.repositories;
+
+import de.aperepublic.server.Entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Integer> {
 }
