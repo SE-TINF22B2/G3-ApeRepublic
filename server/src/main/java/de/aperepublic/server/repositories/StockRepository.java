7cfcd8f9f3015205383b07d558fcbc060f1efcb0
Anisa Aburass <anisa.aburass@gmail.com>
2024-05-15 16:16:02 +0200

    Test hinzugefügt

--- server/src/main/java/de/aperepublic/server/repositories/StockRepository.java
+++ server/src/main/java/de/aperepublic/server/repositories/StockRepository.java
@@ -2,9 +2,26 @@ package de.aperepublic.server.repositories;
 
 import de.aperepublic.server.Entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, String> {
+
+    @Query(
+            nativeQuery = true,
+            value = "insert into aperepublic.Stock\n" +
+                    "Values(\"Test\", \"aus\", \"java\", \" ohne explizites Commit\");"
+    )
+    void saveStock();
+
+
+    @Query(
+            nativeQuery = true,
+            value = "insert into aperepublic.Stock\n" +
+                    "Values(\"Test\", \"aus\", \"java\", \"mit Commit\");\n" +
+                    "commit;"
+    )
+    void saveStockWithCommit();
 
 }
