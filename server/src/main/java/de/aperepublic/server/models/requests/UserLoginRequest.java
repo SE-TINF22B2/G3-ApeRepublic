8be4050bcbdcb18167ae104df4925f3789e871e0
Duy Anh Simon Pham <duyanhsimonpham@gmail.com>
2024-05-21 09:03:34 +0200

    Bugfix with annotations in UserLoginRequest.java

--- server/src/main/java/de/aperepublic/server/models/requests/UserLoginRequest.java
+++ server/src/main/java/de/aperepublic/server/models/requests/UserLoginRequest.java
@@ -2,7 +2,13 @@ package de.aperepublic.server.models.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequest {
 
