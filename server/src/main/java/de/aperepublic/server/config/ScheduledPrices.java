212a70ee1e908aeb9669482152a886af2e8b551d
Marc738 <marcelia@web.de>
2024-06-19 10:01:23 +0200

    Rounded to 2

--- server/src/main/java/de/aperepublic/server/config/ScheduledPrices.java
+++ server/src/main/java/de/aperepublic/server/config/ScheduledPrices.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Component
 public class ScheduledPrices {
 
@@ -16,9 +19,13 @@ public class ScheduledPrices {
     public void performTask() {
         if(!priceHistoryTracker.hasEntry(STOCK)) {
             priceHistoryTracker.addEntry(STOCK, 184.29, String.valueOf(System.currentTimeMillis() / 1000));
+            System.out.println(184.29);
         } else {
-            double price = priceHistoryTracker.getLatestPriceOf(STOCK) + (2 * (Math.random() - 0.5));
+            BigDecimal bd = new BigDecimal(priceHistoryTracker.getLatestPriceOf(STOCK) + (2 * (Math.random() - 0.4)));
+            bd = bd.setScale(2, RoundingMode.HALF_UP);
+            double price = bd.doubleValue();
             priceHistoryTracker.addEntry(STOCK, price, String.valueOf(System.currentTimeMillis() / 1000));
+            System.out.println(price);
         }
 
     }
