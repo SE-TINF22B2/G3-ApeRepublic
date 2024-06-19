800bf5e677d0b242bc02ca0bec1aab1db79f4f5a
Marc738 <marcelia@web.de>
2024-06-19 10:11:23 +0200

    Multiple stocks

--- server/src/main/java/de/aperepublic/server/config/ScheduledPrices.java
+++ server/src/main/java/de/aperepublic/server/config/ScheduledPrices.java
@@ -6,27 +6,37 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ScheduledPrices {
 
-    private final String STOCK = "AAPL";
+    private final ArrayList<StockTemplate> STOCKS = new ArrayList<>(List.of(new StockTemplate("AAPL", 184.29), new StockTemplate("MSFT", 416.96), new StockTemplate("NVDA", 129.90)));
 
     @Autowired
     public PriceHistoryTracker priceHistoryTracker;
 
     @Scheduled(fixedRate = 2000)
     public void performTask() {
-        if(!priceHistoryTracker.hasEntry(STOCK)) {
-            priceHistoryTracker.addEntry(STOCK, 184.29, String.valueOf(System.currentTimeMillis() / 1000));
-            System.out.println(184.29);
-        } else {
-            BigDecimal bd = new BigDecimal(priceHistoryTracker.getLatestPriceOf(STOCK) + (2 * (Math.random() - 0.4)));
-            bd = bd.setScale(2, RoundingMode.HALF_UP);
-            double price = bd.doubleValue();
-            priceHistoryTracker.addEntry(STOCK, price, String.valueOf(System.currentTimeMillis() / 1000));
-            System.out.println(price);
+        for (StockTemplate stock : STOCKS) {
+            if(!priceHistoryTracker.hasEntry(stock.name)) {
+                priceHistoryTracker.addEntry(stock.name, stock.initValue, String.valueOf(System.currentTimeMillis() / 1000));
+            } else {
+                BigDecimal bd = new BigDecimal(priceHistoryTracker.getLatestPriceOf(stock.name) + (2 * (Math.random() - 0.4)));
+                bd = bd.setScale(2, RoundingMode.HALF_UP);
+                double price = bd.doubleValue();
+                priceHistoryTracker.addEntry(stock.name, price, String.valueOf(System.currentTimeMillis() / 1000));
+            }
         }
+    }
 
+    class StockTemplate {
+        String name;
+        double initValue;
+        public StockTemplate(String name, double value) {
+            this.name = name;
+            this.initValue = value;
+        }
     }
 }
